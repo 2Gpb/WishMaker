d151c119f664922f4077c410a6619435526cf2b7
2Gpb <Pvtyukhmenev@gmail.com>
2024-10-27 17:55:52 +0300

    add location toggle

--- HomeWork2/LocationModule/LocationViewController.swift
+++ HomeWork2/LocationModule/LocationViewController.swift
@@ -6,15 +6,20 @@
 //
 
 import UIKit
+import CoreLocation
 
 class LocationViewController: UIViewController {
     //MARK: - Properties
     private let settingsButton = UIButton(type: .system)
     private let settingsView = UIView()
+    private let locationTextView = UITextView()
+    private let locationToggle = UISwitch()
+    private let locationManager = CLLocationManager()
 
     //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        locationManager.requestWhenInUseAuthorization()
         setUp()
     }
     
@@ -26,8 +31,10 @@ class LocationViewController: UIViewController {
     //MARK: - Setup
     private func setUp() {
         view.backgroundColor = .darkGray
+        setUpLocationTextView()
         setUpSettingsView()
         setUpSettingsButton()
+        setUpLocationToggle()
     }
     
     private func setUpSettingsButton() {
@@ -36,8 +43,8 @@ class LocationViewController: UIViewController {
         settingsButton.tintColor = .white
         settingsButton.addTarget(self, action: #selector(showSettings), for: .touchUpInside)
         view.addSubview(settingsButton)
-        settingsButton.setHeight(40)
-        settingsButton.setWidth(40)
+        settingsButton.setHeight(30)
+        settingsButton.setWidth(30)
         settingsButton.pinTop(to: view.safeAreaLayoutGuide.topAnchor, 15)
         settingsButton.pinRight(to: view.safeAreaLayoutGuide.trailingAnchor, 15)
     }
@@ -58,8 +65,52 @@ class LocationViewController: UIViewController {
         settingsView.setWidth(200)
         settingsView.pinTop(to: view.safeAreaLayoutGuide.topAnchor, 10)
         settingsView.pinRight(to: view.safeAreaLayoutGuide.trailingAnchor, 10)
-        
+    }
+    
+    private func setUpLocationTextView() {
+        locationTextView.translatesAutoresizingMaskIntoConstraints = false
+        locationTextView.backgroundColor = .white
+        locationTextView.layer.cornerRadius = 20
+        locationTextView.isUserInteractionEnabled = false
+        view.addSubview(locationTextView)
+        locationTextView.pinTop(to: view.safeAreaLayoutGuide.topAnchor, 60)
+        locationTextView.pinRight(to: view.safeAreaLayoutGuide.trailingAnchor, 15)
+        locationTextView.pinLeft(to: view.safeAreaLayoutGuide.leadingAnchor, 15)
+        locationTextView.setHeight(300)
+    }
+    
+    private func setUpLocationToggle() {
+        locationToggle.translatesAutoresizingMaskIntoConstraints = false
+        locationToggle.addTarget(self, action: #selector(locationToggleSwitched), for: .touchUpInside)
+        settingsView.addSubview(locationToggle)
+        locationToggle.pinTop(to: settingsView.topAnchor, 50)
+        locationToggle.pinRight(to: settingsView.trailingAnchor, 10)
+    }
+    
+    @objc
+    private func locationToggleSwitched() {
+        if locationToggle.isOn {
+            DispatchQueue.global().async {
+                if CLLocationManager.locationServicesEnabled() {
+                    self.locationManager.delegate = self
+                    self.locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+                    self.locationManager.startUpdatingLocation()
+                } else {
+                    self.locationToggle.setOn(false, animated: true)
+                }
+            }
+        } else {
+            locationTextView.text = ""
+            locationManager.stopUpdatingLocation()
+        }
     }
 }
 
+//MARK: - CLLocationManagerDelegate
+extension LocationViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let coord: CLLocationCoordinate2D = manager.location?.coordinate else { return }
+        locationTextView.text = "Coordinates = \(coord.latitude), \(coord.longitude)"
+    }
+}
 
