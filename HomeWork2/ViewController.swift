83ea2d1fedbfe0c3018a9b7e8828d01e66d274fa
2Gpb <Pvtyukhmenev@gmail.com>
2024-10-27 15:20:01 +0300

    initial commit

---
+++ HomeWork2/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  HomeWork2
+//
+//  Created by Peter on 27.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
+
