cb20c4e5b1d6c7690cc2407141bee64254dd8667
2Gpb <Pvtyukhmenev@gmail.com>
2024-10-27 16:04:38 +0300

    add settings view

--- HomeWork2/LocationModule/LocationViewController.swift
+++ HomeWork2/LocationModule/LocationViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class LocationViewController: UIViewController {
     //MARK: - Properties
     private let settingsButton = UIButton(type: .system)
+    private let settingsView = UIView()
 
     //MARK: - Lifecycle
     override func viewDidLoad() {
@@ -25,18 +26,39 @@ class LocationViewController: UIViewController {
     //MARK: - Setup
     private func setUp() {
         view.backgroundColor = .darkGray
-        setUpSettingssButton()
+        setUpSettingsView()
+        setUpSettingsButton()
     }
     
-    private func setUpSettingssButton() {
+    private func setUpSettingsButton() {
         settingsButton.translatesAutoresizingMaskIntoConstraints = false
         settingsButton.setImage(UIImage(named: "settings"), for: .normal)
         settingsButton.tintColor = .white
+        settingsButton.addTarget(self, action: #selector(showSettings), for: .touchUpInside)
         view.addSubview(settingsButton)
         settingsButton.setHeight(40)
         settingsButton.setWidth(40)
         settingsButton.pinTop(to: view.safeAreaLayoutGuide.topAnchor, 15)
         settingsButton.pinRight(to: view.safeAreaLayoutGuide.trailingAnchor, 15)
+    }
+    
+    @objc
+    private func showSettings() {
+        UIView.animate(withDuration: 0.1) {
+            self.settingsView.alpha = 1 - self.settingsView.alpha
+        }
+    }
+    
+    private func setUpSettingsView() {
+        settingsView.translatesAutoresizingMaskIntoConstraints = false
+        settingsView.backgroundColor = .lightGray
+        settingsView.alpha = 0
+        view.addSubview(settingsView)
+        settingsView.setHeight(300)
+        settingsView.setWidth(200)
+        settingsView.pinTop(to: view.safeAreaLayoutGuide.topAnchor, 10)
+        settingsView.pinRight(to: view.safeAreaLayoutGuide.trailingAnchor, 10)
+        
     }
 }
 
