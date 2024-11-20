308e3cf75e2ccf3203ee332797ea0a4b9b0d768b
2Gpb <Pvtyukhmenev@gmail.com>
2024-11-20 10:47:43 +0300

    add new hw2

---
+++ HomeWork2/HomeWork2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork2
+//
+//  Created by Peter on 20.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
