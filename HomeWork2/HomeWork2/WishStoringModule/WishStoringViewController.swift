456dcbc8714bcb555863aa2a6eb519ca9262a034
2Gpb <Pvtyukhmenev@gmail.com>
2024-11-24 22:19:50 +0300

    hw-3: add tableview

--- HomeWork2/HomeWork2/WishStoringModule/WishStoringViewController.swift
+++ HomeWork2/HomeWork2/WishStoringModule/WishStoringViewController.swift
@@ -8,35 +8,74 @@
 import UIKit
 
 final class WishStoringViewController: UIViewController {
-    // MARK: - Fields
+    // MARK: - Constants
+    private enum Constants {
+        enum CloseButton {
+            static let height: CGFloat = 40
+            static let width: CGFloat = 40
+            static let top: CGFloat = 20
+            static let right: CGFloat = 20
+        }
+        
+        enum Table {
+            static let cornerRadius: CGFloat = 20
+            static let offset: CGFloat = 20
+        }
+    }
+    
+    // MARK: - Private fields
     private let closeButton: UIButton = UIButton(type: .close)
+    private let table: UITableView = UITableView(frame: .zero)
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
-        
         setUp()
     }
     
     // MARK: - Setup
     private func setUp() {
-        setupCloseButton()
+        view.backgroundColor = .white
+        
+        setUpCloseButton()
+        setUpTable()
     }
     
-    private func setupCloseButton() {
+    private func setUpCloseButton() {
         closeButton.addTarget(self, action: #selector (closeButtonTapped), for: .touchUpInside)
         view.addSubview(closeButton)
         
-        closeButton.pinTop(to: view.safeAreaLayoutGuide.topAnchor, 20)
-        closeButton.pinRight(to: view.safeAreaLayoutGuide.trailingAnchor, 20)
-        closeButton.setHeight(40)
-        closeButton.setWidth(40)
+        closeButton.pinTop(to: view.safeAreaLayoutGuide.topAnchor, Constants.CloseButton.top)
+        closeButton.pinRight(to: view.safeAreaLayoutGuide.trailingAnchor, Constants.CloseButton.right)
+        closeButton.setHeight(Constants.CloseButton.height)
+        closeButton.setWidth(Constants.CloseButton.width)
+    }
+    
+    private func setUpTable() {
+        view.addSubview(table)
+        
+        table.backgroundColor = .red
+        table.dataSource = self
+        table.separatorStyle = .none
+        table.layer.cornerRadius = Constants.Table.cornerRadius
+        
+        table.pin(to: view, Constants.Table.offset)
     }
     
     // MARK: - Actions
     @objc
     private func closeButtonTapped() {
         dismiss(animated: true)
+    }
+}
+
+// MARK: - UITableViewDataSource
+extension WishStoringViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        UITableViewCell()
     }
 }
